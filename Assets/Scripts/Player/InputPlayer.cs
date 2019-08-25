c45a8d2aa0f0e8777c91c26579bf4692a2030c6a
Amaya Muniesa <amaya.muniesa@gmail.com>
2019-08-25 18:40:27 +0200

    Subida

---
+++ Assets/Scripts/Player/InputPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour
+{
+
+    [HideInInspector] public float horizontal;
+    [HideInInspector] public float vertical;
+    [HideInInspector] public bool attack;
+    [HideInInspector] public bool ability1;
+    [HideInInspector] public bool ability2;
+    [HideInInspector] public bool inventory;
+    [HideInInspector] public bool interact;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Debug
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        attack = Input.GetButtonDown("Attack");
+        ability1 = Input.GetButtonDown("Ability1");
+        ability2 = Input.GetButtonDown("Ability2");
+        inventory = Input.GetButtonDown("Inventory");
+        interact = Input.GetButtonDown("Interact");
+
+        if (attack == true)
+        {
+            Debug.Log("Ataque " + "Activado");
+        }
+
+        if (ability1 == true)
+        {
+            Debug.Log("Habilidad 1 " + "Activada");
+        }
+
+        if (ability2 == true)
+        {
+            Debug.Log("Habilidad 2 " + "Activada");
+        }
+
+        if (inventory == true)
+        {
+            Debug.Log("Inventario " + "Activado");
+        }
+
+        if (interact == true)
+        {
+            Debug.Log("Interacción " + "Activada");
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputPlayer playerInput;
+    private Transform playerTransform;
+    private float playerSpeed = 3;
+    private float playerHorizontal;
+    private float playerVertical;
+    private Rigidbody2D playerRigidBody;
+
+    // Configurar Animación
+    private bool playerBack;
+    private Animator playerAnimator;
+    private SpriteRenderer playerSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<InputPlayer>();
+        playerTransform = GetComponent<Transform>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+
+        // Configurar Animación
+        playerAnimator = GetComponent<Animator>();
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        playerHorizontal = playerInput.horizontal;
+        playerVertical = playerInput.vertical;
+
+        // Configurar Animación
+        playerBack = (playerVertical > 0);
+
+        AnimationPlayerSprite();
+
+        if (playerHorizontal != 0 || playerVertical != 0)
+        {
+            //playerAnimator.SetBool("Back", playerBack);
+            SetXYAnimator();
+        }
+
+    }
+
+    private void AnimationPlayerSprite()
+    {
+        if (playerHorizontal < 0 && Mathf.Abs(playerVertical) < Mathf.Abs(playerHorizontal))
+        {
+            playerSprite.flipX = true;
+        }
+        else if (playerHorizontal != 0)
+        {
+            playerSprite.flipX = false;
+        }
+    }
+
+    private void SetXYAnimator()
+    {
+        playerAnimator.SetFloat("X", playerHorizontal);
+        playerAnimator.SetFloat("Y", playerVertical);
+    }
+
+    void FixedUpdate()
+    {
+
+        // Movimiento del Personaje Versión 1
+        //Vector2 newPlayerPosition = transformplayer.position + new Vector3(speedplayer * horizontalplayer * Time.deltaTime, speedplayer * verticalplayer * Time.deltaTime, 0);
+        //transformplayer.position = newPlayerPosition;
+
+        // Movimiento del Personaje Versión 2
+        //Vector2 forceplayer = new Vector2(playerHorizontal, playerVertical) * playerSpeed * Time.deltaTime;
+        //playerRigidBody.AddForce(forceplayer);
+
+        // Movimiento del Personaje Versión 3
+        Vector2 forceplayer = new Vector2(playerHorizontal, playerVertical) * playerSpeed;
+        playerRigidBody.velocity = forceplayer;
+
+
+        #region Debug
+        if (playerInput.attack == true)
+        {
+            Debug.Log("Jugador Atacando");
+        }
+
+        if (playerVertical != 0)
+        {
+            Debug.Log("Jugador de Espaldas");
+        }
+        #endregion
+    }
+
+}
